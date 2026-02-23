3f62ede0e81c29506ef218fea58847518d6febfa
Chelsea Alysson Ongjoco <chelsalysongjoco@yahoo.com>
2026-02-23 12:26:19 -0500

    Implemented deposit feature with use case and CLI entry point

---
+++ application/deposit_use_case.py
@@ -0,0 +1,37 @@
+from data_access.transaction_manager import TransactionManager
+from entity import Account, LedgerEntry
+from decimal import Decimal
+
+
+class DepositUseCase:
+    def __init__(self, tx_manager: TransactionManager):
+        self.tx_manager = tx_manager
+
+    def deposit(self, account_id: int, amount: float) -> Account:
+
+        # Checks to make sure the input is positive
+        if amount <= 0:
+            raise ValueError("Deposit amount must be positive")
+
+        with self.tx_manager.begin() as tx:
+            # Fetches the account
+            account = tx.account_repo.get_account_by_id(account_id) 
+
+            if account is None:
+                raise ValueError(f"Account not found: {account_id}")
+
+            # Update balance
+            account.balance += Decimal(str(amount))
+            tx.account_repo.save_account(account)
+
+            # Record ledger entry
+            entry = LedgerEntry(
+                id=0,  # placeholder; DB will generate the real id
+                account_id=account_id,
+                entry_type="deposit",
+                amount=amount,
+            )
+
+            tx.ledger_repo.create_ledger_entry(entry)
+
+            return account
---
+++ deposit.py
@@ -0,0 +1,28 @@
+import argparse
+from context import ApplicationContext
+from application.deposit_use_case import DepositUseCase
+
+
+def main(account_id: int, amount: float):
+
+    # Creates the application context (for the .env, database connections, and making transaction manager)
+    ctx = ApplicationContext()
+    use_case = DepositUseCase(ctx.tx_manager)
+
+    # Run deposit operation
+    updated = use_case.deposit(account_id, amount)
+
+    # Print confirmation
+    print("Deposit complete!")
+    print(f"Account ID: {updated.id}")
+    print(f"Account Name: {updated.name}")
+    print(f"New Balance: {updated.balance:.2f}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("account_id", type=int)
+    parser.add_argument("amount", type=float)
+    args = parser.parse_args()
+
+    main(args.account_id, args.amount)
