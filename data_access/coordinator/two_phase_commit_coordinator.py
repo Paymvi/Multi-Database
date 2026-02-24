f61bbaea35ab97c04fb8a799ea11ebb2ef2ef236
Samrat Raj <110294516+SamratRaj01@users.noreply.github.com>
2026-02-24 13:04:08 -0500

    coordinator commit

--- data_access/coordinator/two_phase_commit_coordinator.py
+++ data_access/coordinator/two_phase_commit_coordinator.py
@@ -1,3 +1,4 @@
+from curses import error
 import uuid
 
 from psycopg import Connection
@@ -17,25 +18,29 @@ class TwoPhaseCommitCoordinator:
 
     def commit(self) -> None:
         '''Commits the transaction across both databases using 2PC.'''
-        # Createa a unique transaction ID for this 2PC transaction
         tx_id = self._new_transaction_id()
         participants = self._participants()
         prepared_participants: list[tuple[str, Connection]] = []
 
+        print(f'Starting 2PC transaction with ID: {tx_id}')
+
         try:
-            # Phase 1: Prepare
+            # Phase 1: PREPARE
             for participant_name, participant_conn in participants:
-                self._execute(participant_conn, 'PREPARE TRANSACTION %s', (tx_id,))
+                with participant_conn.cursor() as cursor:
+                    cursor.execute(f"PREPARE TRANSACTION '{tx_id}'")
                 prepared_participants.append((participant_name, participant_conn))
 
-            # Phase 2: Commit
+
+            # Phase 2: COMMIT PREPARED
             for participant_name, participant_conn in participants:
-                self._execute(participant_conn, 'COMMIT PREPARED %s', (tx_id,))
+                participant_conn.autocommit = True   # critical
+                with participant_conn.cursor() as cursor:
+                    cursor.execute(f"COMMIT PREPARED '{tx_id}'")
+                participant_conn.autocommit = False  # optional: restore
 
         except Exception as error:
-            # Abort: throw out the error since we have a recover command we can run later to manually revocer the transaction if needed
-            print(f"Error handling the transaction: {error}")
-
+            raise RuntimeError(f"Error handling the transaction: {error}")
 
     #Hardcoded participant list for now but we can make it dynamic later
     def _participants(self) -> list[tuple[str, Connection]]:
@@ -45,7 +50,7 @@ class TwoPhaseCommitCoordinator:
         ]
 
     @staticmethod
-    def _execute(conn: Connection, query: str, params: tuple | None = None) -> None:
+    def _execute(conn: Connection, query: str, params: any | None = None) -> None:
         with conn.cursor() as cursor:
             cursor.execute(query, params)
 
