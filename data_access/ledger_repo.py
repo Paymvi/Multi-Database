ca01f961aef20d2402812e92ac10ff82d75bedae
Samrat Raj <110294516+SamratRaj01@users.noreply.github.com>
2026-02-24 13:10:50 -0500

    add ledger_repo create_ledger_entry implementation

--- data_access/ledger_repo.py
+++ data_access/ledger_repo.py
@@ -10,7 +10,11 @@ class LedgerRepository:
 
     def create_ledger_entry(self, entry: LedgerEntry) -> None:
         '''Creates a new ledger entry in the database.'''
-        ...
+        with self._conn.cursor() as cursor:
+            cursor.execute(
+                "INSERT INTO ledger (account_id, type, amount) VALUES (%s, %s, %s)",
+                (entry.account_id, entry.entry_type.value, entry.amount),
+            )
 
     def get_ledger_entries_by_account_id(self, account_id: int) -> list[LedgerEntry]:
         '''Retrieves all ledger entries for a given account ID.'''
